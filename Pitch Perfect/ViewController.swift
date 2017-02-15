8a21e056c3cf256a60c5b1960060449885cda0b7
SahilDhawan <sahildhawan2011@gmail.com>
2017-02-16 00:09:38 +0530

    Created UI and added required images to Assets

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        self.stopRecordingButton.isEnabled = true
+        self.recordButton.isEnabled = false
+        self.recordingLabel.text = "Recording in Progress"
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        self.recordButton.isEnabled = true
+        self.stopRecordingButton.isEnabled = false
+        self.recordingLabel.text = "Tap to Record"
+
+        
+    }
 
 }
 
